88f0f53d8ff8d31e5cb503c75018aca5b7a0b922
Michael "Z" Goddard <mzgoddard@gmail.com>
2016-04-21 22:44:23 -0400

    fixup! Implement Animateds with transform transition

--- src/modules/animated-agent/index.jsx
+++ src/modules/animated-agent/index.jsx
@@ -9,6 +9,24 @@ import AnimateCallbackOptions from './animate-callback-options';
 import AnimateTimer from './animate-timer';
 import Rect from './rect';
 
+/**
+ * AnimatedAgent
+ *
+ * Work with Animated wrapped React components to animate around the screen.
+ *
+ * AnimatedAgent and Animated can be used very simply to have a React component
+ * animate from one area of the window to another area. By default an Animated
+ * triggers an animation with the agent any time it goes through its render
+ * lifecycle. For easy use the default animation will animate linearly from its
+ * last location to the current location.
+ *
+ * Animateds can animate a React component from a previous hierarchy to a new
+ * hierarchy. Say you have two lists and want to animate one of the list items
+ * from one list to the other. As long as the old React element and the new one
+ * in the new list has an Animated wrapping element with the same animateKey,
+ * the agent will use the last remembered position and start an animation with
+ * that last position and the new one.
+ */
 export default class AnimationAgent extends Component {
   constructor(...args) {
     super(...args);
